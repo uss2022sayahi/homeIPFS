91293b4905eafdf9729181d1d0ac18f520995ac0
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-06-17 12:50:15 -0400

    Update IP command

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 IPFS_PATH=~/.ipfs ipfs bootstrap rm --all
 IPFS_PATH=~/.ipfs ipfs config show
 echo "Use the following information to bootstrap the network:"
-hostname -I
+ip -4 addr | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f6
 IPFS_PATH=~/.ipfs ipfs config show | grep "PeerID"
 echo "In order to bootstrap, use the above outputs (when run on the bootstrap node), using this command:"
 echo "IPFS_PATH=~/.ipfs ipfs bootstrap add /ip4/<IP of boostrap node>/tcp/4001/ipfs/<PeerID of boostrap node>"
